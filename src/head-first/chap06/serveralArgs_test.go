078b67b76664e87994bf5e37054b0b1428eeac2b
sjsdfg <736777445@qq.com>
2020-06-26 15:55:16 +0800

    feature: slice params

--- src/head-first/chap06/serveralArgs_test.go
+++ src/head-first/chap06/serveralArgs_test.go
@@ -11,6 +11,12 @@ func TestSeveral(t *testing.T) {
 	assert.Equalf(t, max, 4.5, "error message %s", "formatted")
 }
 
+func TestSlice(t *testing.T) {
+	float64s := []float64{1.2, 2.3, 3.4, 4.5}
+	max := findMax(float64s...)
+	assert.Equal(t, max, 4.5)
+}
+
 func findMax(values ...float64) float64 {
 	max := math.Inf(-1)
 	for _, val := range values {
